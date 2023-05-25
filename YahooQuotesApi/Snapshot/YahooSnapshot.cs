7b92ff2fae90d4a6c724ec7321cf0e2d56b76c1b
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2023-05-25 20:11:24 -0300

    WIP: getting cookie and crumb

--- YahooQuotesApi/Snapshot/YahooSnapshot.cs
+++ YahooQuotesApi/Snapshot/YahooSnapshot.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -33,16 +34,20 @@ public sealed class YahooSnapshot : IDisposable
     private async Task<Dictionary<Symbol, Security?>> Producer(Symbol[] symbols, CancellationToken ct)
     {
         Dictionary<Symbol, Security?> dict = symbols.ToDictionary(s => s, s => (Security?)null);
+
         if (!symbols.Any())
             return dict;
+
         IEnumerable<JsonElement> elements = await GetElements(symbols, ct).ConfigureAwait(false);
-      
+
         foreach (JsonElement element in elements)
         {
             Security security = new(element, Logger);
             Symbol symbol = security.Symbol;
+
             if (!dict.ContainsKey(symbol))
                 throw new InvalidOperationException(symbol.Name);
+
             dict[symbol] = security;
         }
 
@@ -51,8 +56,25 @@ public sealed class YahooSnapshot : IDisposable
 
     private async Task<IEnumerable<JsonElement>> GetElements(Symbol[] symbols, CancellationToken ct)
     {
+        // get cookie and crumb value and send it to get uri?
+
+        HttpClient httpClient = HttpClientFactory.CreateClient();
+        Uri fcUrl = new("https://fc.yahoo.com/");
+        using HttpResponseMessage response1 = await httpClient.GetAsync(fcUrl, ct).ConfigureAwait(false);
+        response1.Headers.TryGetValues("Set-Cookie", out var cookieValue);
+
+        Uri crumbUrl = new("https://query2.finance.yahoo.com/v1/test/getcrumb");
+        using HttpRequestMessage request = new(HttpMethod.Get, crumbUrl);
+        request.Headers.Add("cookie", cookieValue);
+        using HttpResponseMessage response = await httpClient.GetAsync(crumbUrl, ct).ConfigureAwait(false);
+
+        response.EnsureSuccessStatusCode();
+        string crumb = await response.Content.ReadAsStringAsync(ct).ConfigureAwait(false);
+
+        //
+
         (Uri uri, List<JsonElement> elements)[] datas =
-            GetUris(YahooQuotesBuilder.BaseUrl, symbols)
+            GetUris(YahooQuotesBuilder.BaseUrl, symbols, crumb)
                 .Select(uri => (uri, elements: new List<JsonElement>()))
                 .ToArray();
 
@@ -63,26 +85,27 @@ public sealed class YahooSnapshot : IDisposable
         };
 
         await Parallel.ForEachAsync(datas, parallelOptions, async (data, ct) =>
-            data.elements.AddRange(await MakeRequest(data.uri, ct).ConfigureAwait(false))).ConfigureAwait(false);
+            data.elements.AddRange(await MakeRequest(data.uri, cookieValue, ct).ConfigureAwait(false))).ConfigureAwait(false);
 
         return datas.Select(x => x.elements).SelectMany(x => x);
     }
 
-    private IEnumerable<Uri> GetUris(string baseUrl, IEnumerable<Symbol> symbols)
+    private IEnumerable<Uri> GetUris(string baseUrl, IEnumerable<Symbol> symbols, string crumb)
     {
         return symbols
             .Select(symbol => WebUtility.UrlEncode(symbol.Name))
             .Chunk(100)
-            .Select(s => $"{baseUrl}{string.Join(",", s)}")
+            .Select(s => $"{baseUrl}{string.Join(",", s)}&crumb={crumb}")
             .Select(s => new Uri(s));
     }
 
-    private async Task<JsonElement[]> MakeRequest(Uri uri, CancellationToken ct)
+    private async Task<JsonElement[]> MakeRequest(Uri uri, IEnumerable<string> cookieValue, CancellationToken ct)
     {
         Logger.LogInformation("{Uri}", uri.ToString());
 
         HttpClient httpClient = HttpClientFactory.CreateClient("snapshot");
         httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        httpClient.DefaultRequestHeaders.Add("Cookie", cookieValue);
 
         //Don't use GetFromJsonAsync() or GetStreamAsync() because it would throw an exception
         //and not allow reading a json error messages such as NotFound.
@@ -92,8 +115,10 @@ public sealed class YahooSnapshot : IDisposable
 
         if (!jsonDocument.RootElement.TryGetProperty("quoteResponse", out JsonElement quoteResponse))
             throw new InvalidDataException("quoteResponse");
+
         if (!quoteResponse.TryGetProperty("error", out JsonElement error))
             throw new InvalidDataException("error");
+
         if (error.ValueKind is not JsonValueKind.Null)
         {
             string errorMessage = error.ToString();
@@ -105,8 +130,10 @@ public sealed class YahooSnapshot : IDisposable
             }
             throw new InvalidDataException($"Error requesting YahooSnapshot: {errorMessage}");
         }
+
         if (!quoteResponse.TryGetProperty("result", out JsonElement result))
             throw new InvalidDataException("result");
+
         return result.EnumerateArray().ToArray();
     }
 
